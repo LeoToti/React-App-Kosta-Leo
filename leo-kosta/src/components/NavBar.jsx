95e50a8903c46c1f4dc8733d0752f99410d28d8e
LeoToti <48243978+LeoToti@users.noreply.github.com>
2021-04-28 16:16:24 +0200

    five

---
+++ leo-kosta/src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+
+
+
+
+
+import {Navbar, Nav, Form, Button, FormControl} from 'react-bootstrap'
+
+
+
+
+
+const NavBar = () => (
+
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" id="search" onChange="handleChange"/>
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+
+
+
+
+
+)
+
+export default NavBar
